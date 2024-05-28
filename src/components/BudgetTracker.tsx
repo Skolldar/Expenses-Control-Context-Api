5a0bd493e9310bceda751dd0d3ce36c88a8e60dc
Carla Pena <exoreiba@gmail.com>
2024-05-28 15:46:54 +0200

    added estilos y la imagen del grafico

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,7 +1,14 @@
 
 const BudgetTracker = () => {
   return (
-    <div>BudgetTracker</div>
+    <>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="flex justify-center">
+            <img src="/grafico.jpg" alt="Grafico"/>
+
+        </div>
+    </div>
+    </>
   )
 }
 
