044c40a5e47cb2681d2f418acbe3d7dc19450b97
Carla Pena <exoreiba@gmail.com>
2024-05-29 13:43:08 +0200

    agregamos la ventana modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react"
 import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
 function App() {
   //con useContext accedemos a todo lo que retorna el value.
   //Aunque la mejor forma de hacerlo es atraves de un custom hook:
@@ -16,14 +17,20 @@ function App() {
   return (
     <>
       <header className="bg-teal-600 bg-fixed py-20">
-        <h1 className="max-w-5xl mx-auto font-bold text-white pb-10 text-6xl">Control.<span className="text-xl">Budget</span></h1>
+        <h1 className="max-w-5xl mx-auto font-bold text-white pb-10 text-7xl">Control<span className="text-4xl">.Budget</span></h1>
       </header>
 
-      <div className="translate-y-[-3rem] max-w-3xl mx-auto bg-white shadow-lg rounded-3xl p-10">
+      <div className="translate-y-[-5rem] max-w-3xl mx-auto bg-white shadow-lg rounded-3xl p-10">
         {isValidBudget 
           ? <BudgetTracker/> 
           : <BudgetForm />}
       </div>
+
+  {isValidBudget && (
+    <main className="max-w-3xl mx-auto py-10">
+      <ExpenseModal/>
+    </main>
+  )}
     </>
   )
 }
