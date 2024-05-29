cf1511fad6a0e61edec9ecb7920b5a90669187be
Carla Pena <exoreiba@gmail.com>
2024-05-29 14:11:51 +0200

    agregamos el accion el show-modal, agg el modal:boolean y en el initialstate el modal : false. ya que al darle la action el modal cambiara a true para mostrarlo

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,14 +1,20 @@
+
 //Acciones:
 export type BudgetActions = 
-{type : 'add-budget', payload: {budget: number}}
+{type : 'add-budget', payload: {budget: number}} |
+{type: 'show-modal'}
 
 //state local:
-export type BudgetState = 
-{budget : number}
+export type BudgetState = {
+    budget : number,
+    modal:boolean
+}
 
 //se le sincronizza con budgetstate
-export const initialState : BudgetState = 
-{budget : 0}
+export const initialState : BudgetState = {
+    budget : 0,
+    modal: false
+}
 
 
 //reducer:
@@ -20,6 +26,13 @@ export const budgetReducer = (
         return {
             ...state,
             budget : action.payload.budget
+        }
+    }
+
+    if(action.type === 'show-modal') {
+        return {
+            ...state,
+            modal: true
         }
     }
     return state
