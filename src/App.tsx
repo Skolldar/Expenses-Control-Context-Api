8e994ea2742db50b461f3358f42fed3951dc7e91
Carla Pena <exoreiba@gmail.com>
2024-06-25 10:15:10 +0200

    create new component ExpenseList..  and added after validBudget

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseModal from "./components/ExpenseModal"
+import ExpenseList from "./components/ExpenseList"
 function App() {
   //con useContext accedemos a todo lo que retorna el value.
   //Aunque la mejor forma de hacerlo es atraves de un custom hook:
@@ -28,6 +29,7 @@ function App() {
 
   {isValidBudget && (
     <main className="max-w-3xl mx-auto py-10">
+      <ExpenseList/>
       <ExpenseModal/>
     </main>
   )}
