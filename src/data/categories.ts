9dfdfe68003102ff5cc02eac8b05f379105c9325
Carla Pena <exoreiba@gmail.com>
2024-06-25 10:50:00 +0200

    changed again names

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -5,7 +5,7 @@ export const categories : Category [] = [
     { id: '2', name: 'Food', icon: 'food' },
     { id: '3', name: 'House', icon: 'house' },
     { id: '4', name: 'Miscellaneous Expenses', icon: 'expenses' },
-    { id: '5', name: 'Hobby', icon: 'hobby' },
+    { id: '5', name: 'Entertainment', icon: 'entertainment' },
     { id: '6', name: 'Health', icon: 'health' },
     { id: '7', name: 'Subscriptions', icon: 'subscriptions' },
   ];
