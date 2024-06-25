e72ba6c6534228e393f014f78f8bd6e2f6196436
Carla Pena <exoreiba@gmail.com>
2024-06-25 10:41:02 +0200

    imported useMemo and categories.
    Filter categories to return each categories name selected in the modal form new expense.
    
    added style to categoriesname

--- src/components/ExpenseDetails.tsx
+++ src/components/ExpenseDetails.tsx
@@ -1,6 +1,8 @@
+import { useMemo } from "react"
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailsProps = {
     expense : Expense
@@ -8,6 +10,10 @@ type ExpenseDetailsProps = {
 
 
 const ExpenseDetails = ({expense} : ExpenseDetailsProps) => {
+  //Filtar y retornar el nombre despues de selecionarlo en el form.
+  const   categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+
+
   return (
     <>
     <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
@@ -16,6 +22,7 @@ const ExpenseDetails = ({expense} : ExpenseDetailsProps) => {
       </div>
 
       <div>
+        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
         <p>{expense.expenseName}</p>                            
         <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
       </div>
