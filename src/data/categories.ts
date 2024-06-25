d4db7dd9b65a6e3172f13603cefb7d5d12b5be93
Carla Pena <exoreiba@gmail.com>
2024-06-25 10:28:12 +0200

    Changed to english my categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,11 +1,11 @@
 import type { Category } from "../types";
 
 export const categories : Category [] = [
-    { id: '1', name: 'Ahorro', icon: 'ahorro' },
-    { id: '2', name: 'Comida', icon: 'comida' },
-    { id: '3', name: 'Casa', icon: 'casa' },
-    { id: '4', name: 'Gastos Varios', icon: 'gastos' },
-    { id: '5', name: 'Ocio', icon: 'ocio' },
-    { id: '6', name: 'Salud', icon: 'salud' },
-    { id: '7', name: 'Suscripciones', icon: 'suscripciones' },
+    { id: '1', name: 'Savings', icon: 'savings' },
+    { id: '2', name: 'Food', icon: 'food' },
+    { id: '3', name: 'House', icon: 'house' },
+    { id: '4', name: 'Miscellaneous Expenses', icon: 'expenses' },
+    { id: '5', name: 'Leisure', icon: 'leisure' },
+    { id: '6', name: 'Health', icon: 'health' },
+    { id: '7', name: 'Subscriptions', icon: 'subscriptions' },
   ];
