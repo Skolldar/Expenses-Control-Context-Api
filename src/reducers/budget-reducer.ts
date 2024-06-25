53a32ee6d689e9a09287f893ccae132d8a5868bc
Carla Pena <exoreiba@gmail.com>
2024-06-25 10:32:52 +0200

    Imported and installed uuidv4 imported types "DraftExpense and Expense"
    added the budgetaction type add-expense ...darftexpense
    
    added in the staate local with a empty Expense the same in the initialState
    
    created a  create DraftExpense  with copy draft and return generated id. with uuidv4
    
    created an action for add expense and take a copy of state, state.expense and expense
    
    added modal: false. to closed when you make a click on add new expense in the modal form

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,23 +1,34 @@
+import {v4 as uuidv4} from 'uuid';
+
+import { DarftExpense, Expense } from "../types"
 
 //Acciones:
 export type BudgetActions = 
 {type : 'add-budget', payload: {budget: number}} |
 {type: 'show-modal'} |
-{type: 'close-modal'}
+{type: 'close-modal'} |
+{type: 'add-expense', playload: {expense: DarftExpense}}
 
 //state local:
 export type BudgetState = {
     budget : number,
     modal:boolean,
-
+    expenses: Expense[]
 }
 
 //se le sincronizza con budgetstate
 export const initialState : BudgetState = {
     budget : 0,
-    modal: false
+    modal: false,
+    expenses: []
 }
 
+const createExpense = (draftExpense: DarftExpense) : Expense => {
+    return {
+        ...draftExpense,
+        id: uuidv4()
+    }
+}
 
 //reducer:
 export const budgetReducer = (
@@ -41,6 +52,16 @@ export const budgetReducer = (
     if(action.type === 'close-modal') {
         return {
             ...state,
+            modal: false
+        }
+    }
+
+    if(action.type === 'add-expense') {action.playload.expense
+        const expense = createExpense(action.playload.expense) //de esta forma sabe que tiene qe tomar un gasto sin ID y regresarlo con ID
+
+        return {
+            ...state,
+            expenses: [...state.expenses, expense ], //action.payload.expense para que lo agrege ya que se pasa como un DRAFT
             modal: false
         }
     }
