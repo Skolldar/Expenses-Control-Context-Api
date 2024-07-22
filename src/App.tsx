266bba592e53b0fae900e801360de47807a6d494
Skolldar <exoreiba@gmail.co>
2024-07-22 17:58:09 +0200

    imported filteredBycategory added in the  page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseModal from "./components/ExpenseModal"
 import ExpenseList from "./components/ExpenseList"
+import FilterByCategory from "./components/FilterByCategory"
 
 function App() {
   //con useContext accedemos a todo lo que retorna el value.
@@ -38,6 +39,7 @@ function App() {
 
   {isValidBudget && (
     <main className="max-w-3xl mx-auto py-10">
+      <FilterByCategory/>
       <ExpenseList/>
       <ExpenseModal/>
     </main>
